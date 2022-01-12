3a5cf4a05c1da615117d4cbdca8a03abc5c6e546
Miruna Mester <miruraluale@gmail.com>
2022-01-12 17:40:06 +0200

    alembic upgradeee

---
+++ alembic/versions/c428a5b48c95_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: c428a5b48c95
+Revises: 88b47541e196
+Create Date: 2022-01-12 17:45:43.830357
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c428a5b48c95'
+down_revision = '88b47541e196'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
